65597e00784b1cbea83072518dab3cd7644b4ff5
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-01 17:37:06 +0800

    feat(routes): api

--- src/routes/oauth2.js
+++ src/routes/oauth2.js
@@ -1,9 +1,24 @@
 import { Router } from 'express'
 import oauth2 from '../config/oauth2orize'
+import { auth } from '../middlewares'
+import { oauthUser, oauthClient } from '../controllers'
+
 const router = Router()
 
-router.get('/authorize', oauth2.authorization)
-router.post('/authorize', oauth2.decision)
-router.post('/token', oauth2.token)
+  .get('/authorize', oauth2.authorization)
+  .post('/authorize', oauth2.decision)
+  .post('/token', oauth2.token)
+
+  .get('/users', auth.isTrustedClient(), oauthUser.index)
+  .post('/users', auth.isTrustedClient(), oauthUser.create)
+  .get('/users/:id', auth.isTrustedClient(), oauthUser.show)
+  .put('/users/:id', auth.isTrustedClient(), oauthUser.update)
+  .delete('/users/:id', auth.isTrustedClient(), oauthUser.destroy)
+
+  .get('/clients', auth.isTrustedClient(), oauthClient.index)
+  .post('/clients', auth.isTrustedClient(), oauthClient.create)
+  .get('/clients/:id', auth.isTrustedClient(), oauthClient.show)
+  .put('/clients/:id', auth.isTrustedClient(), oauthClient.update)
+  .delete('/clients/:id', auth.isTrustedClient(), oauthClient.destroy)
 
 export default () => router
