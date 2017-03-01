bfe33dea87234d14c4b9d136b449b1f5e45f84f6
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-01 17:38:12 +0800

    refactor: remove logger

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose'
-import logger from './logger'
 import config from '../config'
 
 mongoose.Promise = global.Promise
@@ -8,13 +7,13 @@ export default function connectMongoDB () {
   mongoose.connect(config.mongoDB.uri, config.mongoDB.options)
 
   mongoose.connection
-    .on('connected', () => logger.verbose(`Mongoose connection open to ${config.mongoDB.uri}`))
-    .on('disconnected', () => logger.verbose('Mongoose connection disconnected'))
-    .on('error', (err) => logger.error(`Mongoose connection error: ${err}`))
+    .on('connected', () => console.log(`Mongoose connection open to ${config.mongoDB.uri}`))
+    .on('disconnected', () => console.log('Mongoose connection disconnected'))
+    .on('error', (err) => console.error(`Mongoose connection error: ${err}`))
 
   process.on('SIGINT', () => {
     mongoose.connection.close(() => {
-      logger.verbose('Mongoose connection disconnected through app termination')
+      console.log('Mongoose connection disconnected through app termination')
       process.exit(0)
     })
   })
