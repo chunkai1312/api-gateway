6697ae4945657fd0d34f37b49d7d89903ddc1753
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-01 17:40:51 +0800

    refactor: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import main from './main'
-import api from './api'
 import oauth2 from './oauth2'
 
 const router = Router()
 
-router.use('/', main())
-router.use('/api', api())
-router.use('/oauth2', oauth2())
+  .use('/oauth2', oauth2())
+  .use('/', main())
 
 export default () => router
