24d53597bea6d38755d1f400e212ec9e580e6d80
Chun-Kai Wang <chunkai1312@gmail.com>
2017-03-01 12:16:54 +0800

    refactor: app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import 'babel-polyfill'
 
+import path from 'path'
 import express from 'express'
+import ejs from 'ejs'
+import expressLayouts from 'express-ejs-layouts'
 import middlewares, { errorHandler } from './middlewares'
 import routes from './routes'
 import connectMongoDB from './config/mongoose'
+import setupPassport from './config/passport'
 
 connectMongoDB()
+setupPassport()
 
 const app = express()
-  .use(middlewares())
-  .use(routes())
-  .use(errorHandler())
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'html')
+app.engine('html', ejs.renderFile)
+app.use(expressLayouts)
+app.use(express.static(path.resolve(__dirname, '../static')))
+
+app.use(middlewares())
+app.use(routes())
+app.use(errorHandler())
 
 export default app
