3c5327e15633bed32eefbddcdbb110b8dee1f6c3
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-24 15:32:37 +0800

    test(models): oauth_user

--- test/models/oauth_user.spec.js
+++ test/models/oauth_user.spec.js
@@ -1,285 +1,115 @@
-import { expect } from 'chai'
-import co from 'co'
-import { OAuthUser, OAuthClient } from '../../app/models'
 import connectMongoDB from '../../src/config/mongoose'
+import OAuthUser from '../../src/models/oauth_user'
 
 connectMongoDB()
 
 describe('OAuthUser Model:', () => {
-  let client
-  let userSeed
+  const testUser = { username: 'test', password: '123qwe', email: 'test@example.com' }
 
-  before(() => {
-    return co(function * () {
-      client = yield OAuthClient.create({
-        redirectURIs: ['https://www.example.com/callback'],
-        name: 'Test Client'
-      })
-      userSeed = {
-        provider: client.id,
-        username: 'test',
-        password: 'password',
-        email: 'test@example.com',
-        firstName: 'Test',
-        lastName: 'User',
-        passwordResetToken: 'qwertyuiopasdfgh',
-        passwordResetExpires: Date.now() + 3600 * 1000
-      }
-    })
+  beforeEach(async () => {
+    await OAuthUser.remove()
   })
 
-  after(() => {
-    return client.remove()
+  afterEach(async () => {
+    await OAuthUser.remove()
   })
 
   describe('OAuthUserSchema.statics', () => {
     describe('#create()', () => {
-      let user
-
-      before(() => {
-        return OAuthUser.create(userSeed).then(oauthUser => {
-          user = oauthUser
-        })
+      it('should return an OAuthUser with hashed password', async () => {
+        const user = await OAuthUser.create(testUser)
+        expect(user.password).not.toBe(testUser.password)
       })
 
-      after(() => {
-        return user.remove()
+      it('should include hash in hashed password', async () => {
+        const user = await OAuthUser.create(testUser)
+        expect(JSON.parse(user.password)).toHaveProperty('hash')
       })
 
-      it('should return an OAuthUser with hashed password', () => {
-        expect(user.password).to.not.equal('password')
-      })
-
-      it('should include hash in hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('hash')
-      })
-
-      it('should include salt in hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('salt')
+      it('should include salt in hashed password', async () => {
+        const user = await OAuthUser.create(testUser)
+        expect(JSON.parse(user.password)).toHaveProperty('salt')
       })
     })
 
-    describe('#getByUsername()', () => {
-      let user
-
-      before(() => {
-        return OAuthUser.create(userSeed).then(oauthUser => {
-          user = oauthUser
-        })
-      })
-
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return an OAuthUser that is equal to get', () => {
-        return OAuthUser.getByUsername('test').then(oauthUser => {
-          expect(oauthUser).to.exist
-          expect(oauthUser.username).to.equal('test')
-        })
+    describe('#findByUsername()', () => {
+      it('should return an OAuthUser that is equal to get', async () => {
+        await OAuthUser.create(testUser)
+        const user = await OAuthUser.findByUsername('test')
+        expect(user).toBeDefined()
+        expect(user.username).toBe('test')
       })
     })
 
-    describe('#getByEmail()', () => {
-      let user
-
-      before(() => {
-        return OAuthUser.create(userSeed).then(oauthUser => {
-          user = oauthUser
-        })
-      })
-
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return an OAuthUser that is equal to get', () => {
-        return OAuthUser.getByEmail('test@example.com').then(oauthUser => {
-          expect(oauthUser).to.exist
-          expect(oauthUser.email).to.equal('test@example.com')
-        })
+    describe('#findByEmail()', () => {
+      it('should return an OAuthUser that is equal to get', async () => {
+        await OAuthUser.create(testUser)
+        const user = await OAuthUser.findByEmail('test@example.com')
+        expect(user).toBeDefined()
+        expect(user.email).toBe('test@example.com')
       })
     })
 
-    describe('#getByUsernameOrEmail()', () => {
-      let user
-
-      before(() => {
-        return OAuthUser.create(userSeed).then(oauthUser => {
-          user = oauthUser
-        })
+    describe('#findByUsernameOrEmail()', () => {
+      it('should return an OAuthUser when the username exists', async () => {
+        await OAuthUser.create(testUser)
+        const user = await OAuthUser.findByUsernameOrEmail('test')
+        expect(user).toBeDefined()
+        expect(user.username).toBe('test')
       })
 
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return an OAuthUser when the username exists', () => {
-        return OAuthUser.getByUsernameOrEmail('test').then(oauthUser => {
-          expect(oauthUser).to.exist
-          expect(oauthUser.username).to.equal('test')
-        })
-      })
-
-      it('should return an OAuthUser when the email exists', () => {
-        return OAuthUser.getByUsernameOrEmail('test@example.com').then(oauthUser => {
-          expect(oauthUser).to.exist
-          expect(oauthUser.email).to.equal('test@example.com')
-        })
-      })
-    })
-
-    describe('#getByPasswordResetToken()', () => {
-      let user
-
-      before(() => {
-        return OAuthUser.create(userSeed).then(oauthUser => {
-          user = oauthUser
-        })
-      })
-
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return an OAuthUser that is equal to get', () => {
-        return OAuthUser.getByPasswordResetToken('qwertyuiopasdfgh').then(oauthUser => {
-          expect(oauthUser).to.exist
-          expect(oauthUser.passwordResetToken).to.equal('qwertyuiopasdfgh')
-          expect(oauthUser.passwordResetExpires).to.above(Date.now())
-        })
+      it('should return an OAuthUser when the email exists', async () => {
+        await OAuthUser.create(testUser)
+        const user = await OAuthUser.findByUsernameOrEmail('test@example.com')
+        expect(user).toBeDefined()
+        expect(user.email).toBe('test@example.com')
       })
     })
   })
 
   describe('OAuthUserSchema.methods', () => {
     describe('#save()', () => {
-      let user
-
-      before(() => {
-        return new OAuthUser(userSeed).save().then(oauthUser => {
-          user = oauthUser
-        })
+      it('should return an OAuthUser with hashed password', async () => {
+        const user = await new OAuthUser(testUser).save()
+        expect(user.password).not.toBe('password')
       })
 
-      after(() => {
-        return user.remove()
+      it('should include hash in hashed password', async () => {
+        const user = await new OAuthUser(testUser).save()
+        expect(JSON.parse(user.password)).toHaveProperty('hash')
       })
 
-      it('should return an OAuthUser with hashed password', () => {
-        expect(user.password).to.not.equal('password')
-      })
-
-      it('should include hash in hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('hash')
-      })
-
-      it('should include salt in hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('salt')
+      it('should include salt in hashed password', async () => {
+        const user = await new OAuthUser(testUser).save()
+        expect(JSON.parse(user.password)).toHaveProperty('salt')
       })
     })
 
     describe('#authenticate()', () => {
-      let user
-
-      before(() => {
-        return new OAuthUser(userSeed).save().then(oauthUser => {
-          user = oauthUser
-        })
+      it('should return true if password is valid', async () => {
+        const user = await new OAuthUser(testUser).save()
+        const isAuthenticated = await user.authenticate('123qwe')
+        expect(isAuthenticated).toBe(true)
       })
 
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return true if password is valid', () => {
-        return user.authenticate('password').then((isAuthenticated) => {
-          expect(isAuthenticated).to.be.true
-        })
-      })
-
-      it('should return false if password is invalid', () => {
-        return user.authenticate('blah').then((isAuthenticated) => {
-          expect(isAuthenticated).to.be.false
-        })
+      it('should return false if password is invalid', async () => {
+        const user = await new OAuthUser(testUser).save()
+        const isAuthenticated = await user.authenticate('blah')
+        expect(isAuthenticated).toBe(false)
       })
     })
 
     describe('#changePassword()', () => {
-      let user
-
-      before(() => {
-        return new OAuthUser(userSeed).save().then(oauthUser => {
-          user = oauthUser
-          return user.changePassword('password', 'new')
-        })
+      it('should return true', async () => {
+        const user = await new OAuthUser(testUser).save()
+        const newPassword = await user.changePassword('123qwe', 'new')
+        expect(newPassword).toBe(true)
       })
 
-      after(() => {
-        return user.remove()
-      })
-
-      it('should return an OAuthUser with new hashed password', () => {
-        expect(user.password).to.not.equal('password')
-        expect(user.password).to.not.equal('new')
-      })
-
-      it('should include hash in new hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('hash')
-      })
-
-      it('should include salt in new hashed password', () => {
-        expect(JSON.parse(user.password)).to.have.property('salt')
-      })
-
-      it('should return true if new password is valid', () => {
-        return user.authenticate('new').then((isAuthenticated) => {
-          expect(isAuthenticated).to.be.true
-        })
-      })
-
-      it('should return false if new password is invalid', () => {
-        return user.authenticate('password').then((isAuthenticated) => {
-          expect(isAuthenticated).to.be.false
-        })
-      })
-
-      it('should return false if old password is invalid', () => {
-        user.changePassword('password', 'blah').then((res) => {
-          return expect(res).to.be.false
-        })
-      })
-    })
-
-    describe('#generatePasswordResetToken()', () => {
-      let user
-      let passwordResetToken
-      let passwordResetExpires
-
-      before(() => {
-        return new OAuthUser(userSeed).save().then(oauthUser => {
-          user = oauthUser
-        })
-      })
-
-      after(() => {
-        return user.remove()
-      })
-
-      it('should generate password reset token', () => {
-        return user.generatePasswordResetToken().then(() => {
-          passwordResetToken = user.passwordResetToken
-          passwordResetExpires = user.passwordResetExpires
-          expect(user.passwordResetToken).to.exist
-          expect(user.passwordResetExpires).to.exist
-        })
-      })
-
-      it('should generate new password reset token', () => {
-        return user.generatePasswordResetToken().then(() => {
-          expect(user.passwordResetToken).to.not.equal(passwordResetToken)
-          expect(user.passwordResetExpires).to.not.equal(passwordResetExpires)
-        })
+      it('should return false if old password is invalid', async () => {
+        const user = await new OAuthUser(testUser).save()
+        const newPassword = await user.changePassword('error', 'new')
+        expect(newPassword).toBe(false)
       })
     })
   })
