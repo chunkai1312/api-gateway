b00fcfe576cadc91b793fc75c84eaa9434f8b67b
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-24 16:04:11 +0800

    test(models): oauth_client

--- test/models/oauth_client.spec.js
+++ test/models/oauth_client.spec.js
@@ -1,67 +1,52 @@
-import { expect } from 'chai'
-import { OAuthClient } from '../../src/models'
 import connectMongoDB from '../../src/config/mongoose'
+import OAuthClient from '../../src/models/oauth_client'
 
 connectMongoDB()
 
 describe('OAuthClient Model:', () => {
-  const clientSeed = {
-    redirectURIs: ['https://www.example.com/callback'],
-    name: 'Test Client'
-  }
+  const testClient = { redirectURIs: ['https://www.example.com/callback'], name: 'Test Client' }
+
+  beforeEach(async () => {
+    await OAuthClient.remove()
+  })
+
+  afterEach(async () => {
+    await OAuthClient.remove()
+  })
 
   describe('OAuthClientSchema.statics', () => {
     describe('#create()', () => {
-      let client
-
-      before(() => {
-        return OAuthClient.create(clientSeed).then(oauthClient => {
-          client = oauthClient
-        })
+      it('should return an OAuthClient with property secret', async () => {
+        const client = await OAuthClient.create(testClient)
+        expect(client.toJSON()).toHaveProperty('secret')
+        expect(client.toObject()).toHaveProperty('secret')
       })
 
-      after(() => {
-        return client.remove()
-      })
-
-      it('should return an OAuthClient with property secret', () => {
-        expect(client).to.have.property('secret')
-        expect(client.secret).to.have.length(32)
-      })
-
-      it('should return an OAuthClient with property trusted that is false', () => {
-        expect(client.trusted).to.be.false
+      it('should return an OAuthClient with property trusted that is false', async () => {
+        const client = await OAuthClient.create(testClient)
+        expect(client.trusted).toBe(false)
       })
     })
   })
 
   describe('OAuthClientSchema.methods', () => {
-    let client
-
-    before(() => {
-      return new OAuthClient(clientSeed).save().then(oauthClient => {
-        client = oauthClient
-      })
-    })
-
-    after(() => {
-      return client.remove()
-    })
-
     describe('#save()', () => {
-      it('should return an OAuthClient with property secret', () => {
-        expect(client).to.have.property('secret')
-        expect(client.secret).to.have.length(32)
+      it('should return an OAuthClient with property secret', async () => {
+        const client = await OAuthClient.create(testClient)
+        expect(client.toJSON()).toHaveProperty('secret')
+        expect(client.toObject()).toHaveProperty('secret')
       })
     })
 
     describe('#hasRedirectURI()', () => {
-      it('should return true if redirectURI is found', () => {
-        expect(client.hasRedirectURI(client.redirectURIs[0])).to.be.true
+      it('should return true if redirectURI is found', async () => {
+        const client = await OAuthClient.create(testClient)
+        expect(client.hasRedirectURI(client.redirectURIs[0])).toBe(true)
       })
 
-      it('should return false if redirectURI is not found', () => {
-        expect(client.hasRedirectURI('http://www.blah.com/callback')).to.be.false
+      it('should return false if redirectURI is not found', async () => {
+        const client = await OAuthClient.create(testClient)
+        expect(client.hasRedirectURI('http://www.blah.com/callback')).toBe(false)
       })
     })
   })
