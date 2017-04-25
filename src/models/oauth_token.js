9f86ad1ca8ec9d7a0bbdd60f3e95e7bead683cdf
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-25 17:31:41 +0800

    refactor(models): oauth_token

--- src/models/oauth_token.js
+++ src/models/oauth_token.js
@@ -1,105 +1,57 @@
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
-import { uid } from 'rand-token'
+import uuid from 'uuid/v4'
 import config from '../config'
 
+export const types = {
+  AUTHORIZATION_CODE: 'AUTHORIZATION_CODE',
+  ACCESS_TOKEN: 'ACCESS_TOKEN',
+  REFRESH_TOKEN: 'REFRESH_TOKEN'
+}
+
+export const createToken = (options = { exp: 3600, sub: '' }) => {
+  return jwt.sign({
+    jti: uuid(),
+    sub: options.sub,
+    exp: Math.floor(Date.now() / 1000) + options.exp
+  }, config.jwt.secret)
+}
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, config.jwt.secret)
+}
+
 const OAuthTokenSchema = new mongoose.Schema({
-  tokenType: { type: String },
-  accessToken: { type: String, unique: true },
-  refreshToken: { type: String },
-  client: { type: mongoose.Schema.Types.ObjectId, ref: 'OAuthClient', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'OAuthUser' },
-  scope: { type: String },
-  expiresAt: { type: Date }
-}, { collection: 'oauth_tokens', timestamps: true })
-
-OAuthTokenSchema.set('toJSON', {
-  transform: (doc, ret, options) => {
-    ret.id = ret._id
-    delete ret._id
-    delete ret.__v
-    delete ret.createdAt
-    delete ret.updatedAt
+  tokenId: { type: String, unique: true },
+  type: { type: String, enum: Object.values(types) },
+  payload: {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'OAuthUser' },
+    clientId: { type: String },
+    redirectURI: { type: String }, // only for authorization code
+    scope: { type: String },
+    expiresAt: { type: Date }
   }
-})
-
-OAuthTokenSchema.virtual('expiresIn').get(function () {
-  return (this.expiresAt.getTime() - this.createdAt.getTime()) / 1000
-})
-
-OAuthTokenSchema.pre('save', function (next) {
-  if (!this.isNew) return next()
-
-  this.tokenType = config.oauth2.tokenType
-  this.accessToken = (config.oauth2.accessToken.isJWT)
-    ? jwt.sign(this.user, config.jwt.secret, { expiresIn: config.oauth2.accessToken.expiresIn })
-    : uid(config.oauth2.accessToken.length)
-
-  this.expiresAt = new Date(
-    this.createdAt.getTime() + config.oauth2.accessToken.expiresIn * 1000
-  )
-
-  if (this.refreshToken === null) return next()
-
-  this.refreshToken = uid(config.oauth2.refreshToken.length)
-  next()
-})
+}, { collection: 'oauth_tokens', timestamps: true })
 
 OAuthTokenSchema.statics = {
 
-  /**
-   * Get an OAuthToken by access token.
-   *
-   * @param  {string}   accessToken - The accessToken for querying.
-   * @param  {Function} callback - The callback function for when query is complete.
-   * @return {Promise}  Result of query.
-   */
-  getByAccessToken (accessToken, callback) {
-    return this.findOne({ accessToken }, callback)
+  saveToken (token, type, payload) {
+    const tokenId = jwt.decode(token).jti
+    return this.create({ tokenId, type, payload })
   },
 
-  /**
-   * Get an OAuthToken by refresh token.
-   *
-   * @param  {string}   refreshToken - The refreshToken for querying.
-   * @param  {Function} callback - The callback function for when query is complete.
-   * @return {Promise}  Result of query.
-   */
-  getByRefreshToken (refreshToken, callback) {
-    return this.findOne({ refreshToken }, callback)
-  }
+  findToken (token) {
+    const tokenId = jwt.decode(token).jti
+    return this.findOne({ tokenId })
+  },
 
+  removeToken (token) {
+    const tokenId = jwt.decode(token).jti
+    return this.findOneAndRemove({ tokenId })
+  }
 }
 
-OAuthTokenSchema.methods = {
+const OAuthToken = mongoose.model('OAuthToken', OAuthTokenSchema)
 
-  /**
-   * Check the access token is valid.
-   *
-   * @return {boolean} Result of check.
-   */
-  isValid () {
-    return new Date() < this.expiresAt
-  },
+export default OAuthToken
 
-  /**
-   * Get seconds for expiration.
-   *
-   * @return {number} seconds.
-   */
-  getExpiresIn () {
-    return Math.floor((this.expiresAt.getTime() - Date.now()) / 1000)
-  },
-
-  /**
-   * Check the token for trusted client.
-   *
-   * @return {boolean} Result of check.
-   */
-  isTrustedClient () {
-    return (!this.user) && (!this.refreshToken)
-  }
-
-}
-
-export default mongoose.model('OAuthToken', OAuthTokenSchema)
