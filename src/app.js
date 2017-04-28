3b16d1438cb0bd45166862d1d83d8a8daaea26cf
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-28 18:25:39 +0800

    refactor: app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import express from 'express'
-import ejs from 'ejs'
-import expressLayouts from 'express-ejs-layouts'
+import ReactEngine from 'react-engine'
 import middlewares, { httpProxy, errorHandler } from './middlewares'
 import routes from './routes'
 import connectMongoDB from './config/mongoose'
@@ -10,14 +9,23 @@ import setupPassport from './config/passport'
 connectMongoDB()
 setupPassport()
 
+global.navigator = global.navigator || {}
+global.navigator.userAgent = global.navigator.userAgent || 'all'
+
 const app = express()
 
 httpProxy(app)
 
+const engine = ReactEngine.server.create({
+  routes: require('./views/routes'),
+  routesFilePath: path.join(__dirname, '/views/routes.js'),
+  performanceCollector: (stats) => console.log(stats)
+})
+
+app.engine('js', engine)
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'html')
-app.engine('html', ejs.renderFile)
-app.use(expressLayouts)
+app.set('view engine', 'js')
+app.set('view', ReactEngine.expressView)
 app.use(express.static(path.resolve(__dirname, '../static')))
 
 app.use(middlewares())
