97e59203d276411dda850c6e954b5057379ccecc
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-29 19:22:22 +0800

    refactor(views): routes

--- src/views/routes.js
+++ src/views/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import Html from './components/Html'
 import Layout from './components/Layout'
 import Login from './components/Login'
@@ -7,11 +7,7 @@ import Signup from './components/Signup'
 import NotFount from './components/NotFount'
 import PasswordForgot from './components/PasswordForgot'
 import PasswordReset from './components/PasswordReset'
-
-
-// import ListPage from './views/list.jsx'
-// import DetailPage from './views/detail.jsx'
-// import Error404 from './views/404.jsx'
+import Authorize from './components/Authorize'
 
 export default (
   <Router history={browserHistory}>
@@ -19,6 +15,7 @@ export default (
       <Route path="/" component={Layout}>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/oauth2/authorize" component={Authorize} />
         <Route path="/password/forgot" component={PasswordForgot} />
         <Route path="/password/reset/:token" component={PasswordReset} />
         <Route path="*" component={NotFount} />
