c40f87d6f37879ae3c6d6ae17e9d79369b7c09a4
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-02 11:33:09 +0800

    refactor: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import oauth2 from '../config/oauth2orize'
-import { auth } from '../middlewares'
+import { authorize } from '../middlewares'
 import { account, user, client, token } from '../controllers'
+
+const wrap = fn => (...args) => fn(...args).catch(args[2])
 
 const router = Router()
 
@@ -11,17 +13,17 @@ const router = Router()
   .get('/oauth2/token/info', token.info)
   .get('/oauth2/token/revoke', token.revoke)
 
-  .get('/oauth2/users', auth.isTrustedClient(), user.index)
-  .post('/oauth2/users', auth.isTrustedClient(), user.create)
-  .get('/oauth2/users/:id', auth.isTrustedClient(), user.show)
-  .put('/oauth2/users/:id', auth.isTrustedClient(), user.update)
-  .delete('/oauth2/users/:id', auth.isTrustedClient(), user.destroy)
+  .get('/oauth2/users', authorize({ scope: 'oauth_user' }), wrap(user.index))
+  .post('/oauth2/users', authorize({ scope: 'oauth_user' }), wrap(user.create))
+  .get('/oauth2/users/:id', authorize({ scope: 'oauth_user' }), wrap(user.show))
+  .put('/oauth2/users/:id', authorize({ scope: 'oauth_user' }), wrap(user.update))
+  .delete('/oauth2/users/:id', authorize({ scope: 'oauth_user' }), wrap(user.destroy))
 
-  .get('/oauth2/clients', auth.isTrustedClient(), client.index)
-  .post('/oauth2/clients', auth.isTrustedClient(), client.create)
-  .get('/oauth2/clients/:id', auth.isTrustedClient(), client.show)
-  .put('/oauth2/clients/:id', auth.isTrustedClient(), client.update)
-  .delete('/oauth2/clients/:id', auth.isTrustedClient(), client.destroy)
+  .get('/oauth2/clients', authorize({ scope: 'oauth_client' }), wrap(client.index))
+  .post('/oauth2/clients', authorize({ scope: 'oauth_client' }), wrap(client.create))
+  .get('/oauth2/clients/:id', authorize({ scope: 'oauth_client' }), wrap(client.show))
+  .put('/oauth2/clients/:id', authorize({ scope: 'oauth_client' }), wrap(client.update))
+  .delete('/oauth2/clients/:id', authorize({ scope: 'oauth_client' }), wrap(client.destroy))
 
   .get('/', account.index)
   .get('/signup', account.showSignup)
