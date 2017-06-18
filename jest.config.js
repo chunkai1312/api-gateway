3b46d400050cf8543f0d5232ed0192d0e08aae3e
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-18 15:42:30 +0800

    chore: jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testRegex: '(/test/.*\\.spec.js)$',
+  testEnvironment: 'node',
+  setupFiles: [
+    '<rootDir>/test/setup.js'
+  ],
+  setupTestFrameworkScriptFile: '<rootDir>/test/setupTestFramework.js',
+  coveragePathIgnorePatterns: ['/src/config/'],
+  coverageReporters: ['text', 'text-summary']
+}
