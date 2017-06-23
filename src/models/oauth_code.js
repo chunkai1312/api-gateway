c37152533a662d718ce6c5f6a34c88b59ee8b316
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-24 00:42:47 +0800

    feat(models): oauth_code

---
+++ src/models/oauth_code.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import mongooseHidden from 'mongoose-hidden'
+import mongooseDelete from 'mongoose-delete'
+
+const OAuthCodeSchema = new mongoose.Schema({
+  code: { type: String },
+  client: { type: mongoose.Schema.Types.ObjectId, ref: 'OAuthClient', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'OAuthUser', required: true },
+  redirectUri: { type: String },
+  scope: { type: String },
+  expiresAt: { type: Date }
+}, { collection: 'oauth_codes', timestamps: true })
+
+OAuthCodeSchema.set('toJSON', { getters: true, virtuals: true })
+OAuthCodeSchema.set('toObject', { getters: true, virtuals: true })
+
+/**
+ * Mongoose Hidden Plugin
+ *
+ * @see https://github.com/mblarsen/mongoose-hidden
+ */
+OAuthCodeSchema.plugin(mongooseHidden(), {
+  hidden: {
+    createdAt: true,
+    updatedAt: true
+  }
+})
+
+/**
+ * Mongoose Delete Plugin
+ *
+ * @see https://github.com/dsanel/mongoose-delete
+ */
+OAuthCodeSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: true
+})
+
+export default mongoose.model('OAuthCode', OAuthCodeSchema)
