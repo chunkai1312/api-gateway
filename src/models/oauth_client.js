f5f6a0ef754e1811169241c97d8ead1c646e4b09
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-24 00:42:09 +0800

    refactor(models): oauth_client

--- src/models/oauth_client.js
+++ src/models/oauth_client.js
@@ -1,58 +1,38 @@
-import { randomBytes } from 'crypto'
 import mongoose from 'mongoose'
 import mongooseHidden from 'mongoose-hidden'
-import uuid from 'uuid/v4'
+import mongooseDelete from 'mongoose-delete'
 
 const OAuthClientSchema = new mongoose.Schema({
   clientId: { type: String, unique: true },
   clientSecret: { type: String },
-  redirectURIs: [{ type: String }],
+  redirectUris: [{ type: String }],
   name: { type: String },
   trusted: { type: Boolean, default: false }
 }, { collection: 'oauth_clients', timestamps: true })
 
-/**
- * @see https://github.com/mblarsen/mongoose-hidden
- */
 OAuthClientSchema.set('toJSON', { getters: true, virtuals: true })
 OAuthClientSchema.set('toObject', { getters: true, virtuals: true })
-OAuthClientSchema.plugin(mongooseHidden())
 
-OAuthClientSchema.pre('save', function (next) {
-  if (!this.isNew) return next()
-  this.clientId = uuid()
-  this.clientSecret = randomBytes(16).toString('hex')
-  return next()
+/**
+ * Mongoose Hidden Plugin
+ *
+ * @see https://github.com/mblarsen/mongoose-hidden
+ */
+OAuthClientSchema.plugin(mongooseHidden(), {
+  hidden: {
+    createdAt: true,
+    updatedAt: true
+  }
 })
 
-OAuthClientSchema.statics = {
+/**
+ * Mongoose Delete Plugin
+ *
+ * @see https://github.com/dsanel/mongoose-delete
+ */
+OAuthClientSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: true
+})
 
-  /**
-   * Find an OAuthClient by client id.
-   *
-   * @param  {string}   clientId - The client id for querying.
-   * @return {Promise}  Result of query.
-   */
-  findByClientId (clientId) {
-    return this.findOne({ clientId })
-  }
-
-}
-
-OAuthClientSchema.methods = {
-
-  /**
-   * Check redirect URI is valid.
-   *
-   * @param  {string} redirectURI - The redirect URI for checking.
-   * @return {boolean} Result of check.
-   */
-  hasRedirectURI (redirectURI) {
-    return this.redirectURIs.includes(redirectURI)
-  }
-
-}
-
-const OAuthClient = mongoose.model('OAuthClient', OAuthClientSchema)
-
-export default OAuthClient
+export default mongoose.model('OAuthClient', OAuthClientSchema)
