9b6739079d5da1bbeaa3b12305d796ef9f0fc6e6
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-27 15:37:23 +0800

    test(services): mailer

--- test/services/mailer.spec.js
+++ test/services/mailer.spec.js
@@ -1,42 +1,39 @@
-const chai = require('chai').use(require('chai-as-promised'))
-const expect = chai.expect
-import mailer from '../../app/services/mailer'
-require('../../app')
+import nodemailer from 'nodemailer'
+import MailerService from '../../src/services/mailer'
 
-describe('mailer', () => {
+const mockTransport = { sendMail: jest.fn(() => Promise.resolve('ok')) }
+nodemailer.createTransport = jest.fn(() => mockTransport)
+
+describe('MailerService', () => {
   describe('#send()', () => {
-    it('should send mail and return message with success', () => {
-      const mailOptions = {
+    it('should send mail and return message with success', async () => {
+      const mailerService = MailerService()
+      const options = {
         to: ['chunkai1312@gmail.com'],
         from: 'no-reply@punwave.com',
         subject: 'Hi there',
         text: 'Awesome sauce',
         html: '<b>Awesome sauce</b>'
       }
-      const result = mailer.send(mailOptions)
-      return expect(result.then(res => res.message)).to.eventually.equal('success')
-    })
-  })
-
-  describe('#sendInvitation()', () => {
-    it('should send invitation mail and return message with success', () => {
-      const invitation = {
-        email: 'chunkai1312@gmail.com',
-        name: 'Chun-Kai Wang',
-        token: 'abcdefghijklmnopqrstuvwxyz'
-      }
-      const result = mailer.sendInvitation(invitation)
-      return expect(result.then(res => res.message)).to.eventually.equal('success')
+      const result = await mailerService.send(options)
+      expect(nodemailer.createTransport).toHaveBeenCalled()
+      expect(mockTransport.sendMail).toHaveBeenCalled()
+      expect(result).toBe('ok')
     })
   })
 
   describe('#sendPasswordReset()', () => {
-    it('should send password reset mail and return message with success', () => {
-      const to = 'chunkai1312@gmail.com'
-      const name = 'Chun-Kai Wang'
-      const url = 'http://campaign.punwave.com/resetPassword?code=xxxxxxxxxxxxxxxx'
-      const result = mailer.sendPasswordReset(to, name, url)
-      return expect(result.then(res => res.message)).to.eventually.equal('success')
+    it('should send password reset mail and return message with success', async () => {
+      const mailerService = MailerService()
+      const spy = jest.spyOn(mailerService, 'send')
+      const user = {
+        profile: { name: 'Test User' },
+        email: 'test@example.com',
+        passwordReset: { token: '1234567890' }
+      }
+      const result = await mailerService.sendPasswordResetEmail(user)
+      expect(spy).toHaveBeenCalled()
+      expect(result).toBe('ok')
     })
   })
 })
