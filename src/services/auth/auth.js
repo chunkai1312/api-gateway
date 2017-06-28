e3a7ce1ce785fc7cfd9f9f75155253d3f42b8095
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-28 13:28:12 +0800

    refactor(services): auth

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -1,62 +1,62 @@
 import { randomBytes } from 'crypto'
 import credential from 'credential'
 import Mailer from '../../services/mailer'
-import OAuthUserModel from '../../models/oauth_user'
+import OAuthUserRepository from '../../repositories/oauth_user'
 
-function AuthService (dependencies = { pw: credential(), mailer: new Mailer(), OAuthUser: OAuthUserModel }) {
+const container = {
+  pw: credential(),
+  mailer: new Mailer(),
+  OAuthUser: OAuthUserRepository()
+}
+
+function AuthService (dependencies = container) {
   const { pw, mailer, OAuthUser } = dependencies
 
   const authService = {}
 
-  const createPasswordResetToken = async (user) => {
+  const generatePasswordResetToken = async (user) => {
     const token = randomBytes(16).toString('hex')
     const expiresAt = Date.now() + (3600 * 1000)
-    user.passwordReset = { token, expiresAt }
-    await user.save()
+    return { token, expiresAt }
   }
 
   authService.createUser = async ({ firstName, lastName, username, email, password }) => {
-    const user = new OAuthUser({ username, email, profile: { firstName, lastName } })
-    user.password = await pw.hash(password)
-    await user.save()
+    const data = { username, password, email, profile: { firstName, lastName } }
+    data.password = await pw.hash(password)
+    const user = await OAuthUser.createUser(data)
     return user
   }
 
-  authService.authenticate = async (login, password) => {
-    const user = await OAuthUser.findOne({ $or: [{ username: login }, { email: login }] })
+  authService.authenticate = async (identifier, password) => {
+    const user = await OAuthUser.getUser(identifier)
     if (!user) return null
     const authenticated = await pw.verify(user.password, password)
     return authenticated ? user : null
   }
 
   authService.forgotPassword = async (email) => {
-    const user = await OAuthUser.findOne({ email })
+    const user = await OAuthUser.getUser(email)
     if (user) {
-      await createPasswordResetToken(user)
+      const passwordReset = await generatePasswordResetToken()
+      user.passwordReset = passwordReset
+      await OAuthUser.save(user)
       mailer.sendPasswordResetEmail(user)
     }
     return user
   }
 
   authService.validatePasswordResetToken = async (token) => {
-    const user = await OAuthUser
-      .findOne({ 'passwordReset.token': token })
-      .where('passwordReset.expiresAt').gt(Date.now())
-
+    const user = await OAuthUser.getUserByPasswordResetToken(token)
     return user
   }
 
   authService.resetPassword = async (token, password) => {
-    const user = await OAuthUser
-      .findOne({ 'passwordReset.token': token })
-      .where('passwordReset.expiresAt').gt(Date.now())
-
+    const user = await OAuthUser.getUserByPasswordResetToken(token)
     if (user) {
       user.password = await pw.hash(password)
       user.passwordReset = {}
-      await user.save()
+      await OAuthUser.save(user)
     }
-
     return user
   }
 
