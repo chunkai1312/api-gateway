bfb14b4577fb977d94dd8030f5b9b86d210b3bc1
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-29 15:24:22 +0800

    test(controllers): home

--- test/controllers/home.spec.js
+++ test/controllers/home.spec.js
@@ -2,14 +2,16 @@ import httpMocks from 'node-mocks-http'
 import HomeController from '../../src/controllers/home'
 
 describe('HomeController', () => {
-  it('should respond 200 with user data', async () => {
-    const req = httpMocks.createRequest({ method: 'GET', url: '/' })
-    const res = httpMocks.createResponse()
+  describe('#index()', () => {
+    it('should respond 200 with user data', async () => {
+      const req = httpMocks.createRequest({ method: 'GET', url: '/' })
+      const res = httpMocks.createResponse()
 
-    const homeController = HomeController()
-    await homeController.index(req, res)
+      const homeController = HomeController()
+      await homeController.index(req, res)
 
-    expect(res._getStatusCode()).toBe(200)
-    expect(res._isJSON()).toBe(true)
+      expect(res._getStatusCode()).toBe(200)
+      expect(res._isJSON()).toBe(true)
+    })
   })
 })
