9226aabedd3da79dfce5a5c3d6d7e2c28e80a1e0
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-30 18:50:09 +0800

    test(controllers): password

--- test/controllers/password.spec.js
+++ test/controllers/password.spec.js
@@ -1,15 +1,41 @@
 import httpMocks from 'node-mocks-http'
+import AuthService from '../../src/services/auth'
 import PasswordController from '../../src/controllers/auth/password'
 
+jest.mock('credential', () => () => ({
+  hash: () => Promise.resolve('hashed-password'),
+  verify: () => Promise.resolve(true)
+}))
+jest.mock('../../src/repositories/oauth_user', () => require('../mocks/repositories/oauth_user'))
+jest.mock('../../src/services/mailer', () => () => ({
+  send: jest.fn(),
+  sendPasswordResetEmail: jest.fn()
+}))
+
+function setup () {
+  const authService = AuthService()
+
+  return { authService }
+}
+
 describe('PasswordController', () => {
+  describe('#PasswordController()', () => {
+    it('should create a PasswordController', () => {
+      const passwordController = PasswordController()
+      expect(passwordController).toBeInstanceOf(Object)
+    })
+  })
+
   describe('#getForgot()', () => {
     it('should render forgot password page', async () => {
+      const { authService } = setup()
+
       const req = httpMocks.createRequest({ method: 'GET', url: '/password/forgot' })
       const res = httpMocks.createResponse()
 
       jest.spyOn(res, 'render')
 
-      const passwordController = PasswordController()
+      const passwordController = PasswordController({ authService })
       await passwordController.getForgot(req, res)
 
       expect(res.render).toHaveBeenCalledWith(req.url)
@@ -18,9 +44,7 @@ describe('PasswordController', () => {
 
   describe('#postForgot()', () => {
     it('should handle the forgot password request', async () => {
-      const mockAuthService = {
-        forgotPassword: jest.fn(() => true)
-      }
+      const { authService } = setup()
 
       const req = httpMocks.createRequest({
         method: 'POST',
@@ -33,17 +57,15 @@ describe('PasswordController', () => {
       })
       const res = httpMocks.createResponse()
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.postForgot(req, res)
 
       expect(req.flash.mock.calls[0][0]).toBe('success')
       expect(res._getRedirectUrl()).toBe('/password/forgot')
     })
 
-    it('should redirect to back when validation error occurs', async () => {
-      const mockAuthService = {
-        forgotPassword: jest.fn(() => true)
-      }
+    it('should redirect to back if validation error occurs', async () => {
+      const { authService } = setup()
 
       const req = httpMocks.createRequest({
         method: 'POST',
@@ -56,7 +78,29 @@ describe('PasswordController', () => {
       })
       const res = httpMocks.createResponse()
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
+      await passwordController.postForgot(req, res)
+
+      expect(req.flash.mock.calls[0][0]).toBe('errors')
+      expect(res._getRedirectUrl()).toBe('/password/forgot')
+    })
+
+    it('should redirect to back if password reset token is invalid', async () => {
+      const { authService } = setup()
+      authService.forgotPassword = jest.fn(() => Promise.resolve(null))
+
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/password/forgot',
+        body: { email: 'test@example.com' },
+        flash: jest.fn(),
+        assert: jest.fn(() => ({ isEmail: jest.fn() })),
+        sanitize: jest.fn(() => ({ normalizeEmail: jest.fn() })),
+        validationErrors: jest.fn(() => false)
+      })
+      const res = httpMocks.createResponse()
+
+      const passwordController = PasswordController({ authService })
       await passwordController.postForgot(req, res)
 
       expect(req.flash.mock.calls[0][0]).toBe('errors')
@@ -66,34 +110,28 @@ describe('PasswordController', () => {
 
   describe('#getReset()', () => {
     it('should render reset password page', async () => {
-      const user = { username: 'test' }
-      const mockAuthService = {
-        validatePasswordResetToken: jest.fn(() => user)
-      }
+      const { authService } = setup()
+      authService.validatePasswordResetToken = jest.fn(() => Promise.resolve({ username: 'test' }))
 
       const req = httpMocks.createRequest({ method: 'GET', url: '/password/reset' })
       const res = httpMocks.createResponse()
 
       jest.spyOn(res, 'render')
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.getReset(req, res)
 
-      expect(res.render).toHaveBeenCalledWith(req.url, user)
+      expect(res.render).toHaveBeenCalledWith(req.url, { username: 'test' })
     })
 
-    it('should redirect to back when password reset token is invalid', async () => {
-      // const user = { username: 'test' }
-      const mockAuthService = {
-        validatePasswordResetToken: jest.fn(() => false)
-      }
+    it('should redirect to back if password reset token is invalid', async () => {
+      const { authService } = setup()
+      authService.validatePasswordResetToken = jest.fn(() => Promise.resolve(null))
 
       const req = httpMocks.createRequest({ method: 'GET', url: '/password/reset', flash: jest.fn() })
       const res = httpMocks.createResponse()
 
-      jest.spyOn(res, 'render')
-
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.getReset(req, res)
 
       expect(res._getRedirectUrl()).toBe('/password/forgot')
@@ -102,72 +140,65 @@ describe('PasswordController', () => {
 
   describe('#postReset()', () => {
     it('should handle the reset password request', async () => {
-      const user = { username: 'test' }
-      const mockAuthService = {
-        resetPassword: jest.fn(() => user)
-      }
+      const { authService } = setup()
 
       const req = httpMocks.createRequest({
         method: 'POST',
         url: '/password/reset',
         body: { password: 'test@example.com', confirmPassword: 'test@example.com' },
-        login: jest.fn(),
+        login: jest.fn((user, callback) => callback()),
         flash: jest.fn(),
         assert: jest.fn(() => ({ len: jest.fn(), equals: jest.fn() })),
         validationErrors: jest.fn(() => false)
       })
       const res = httpMocks.createResponse()
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.postReset(req, res)
 
       expect(req.login).toHaveBeenCalled()
+      expect(res._getRedirectUrl()).toBe('/')
     })
 
-    it('should redirect to back when validation error occurs', async () => {
-      const user = { username: 'test' }
-      const mockAuthService = {
-        resetPassword: jest.fn(() => user)
-      }
+    it('should redirect to back if validation error occurs', async () => {
+      const { authService } = setup()
 
       const req = httpMocks.createRequest({
         method: 'POST',
         url: '/password/reset',
         body: { password: 'test@example.com', confirmPassword: 'test@example.com' },
-        login: jest.fn(),
+        login: jest.fn((user, callback) => callback()),
         flash: jest.fn(),
         assert: jest.fn(() => ({ len: jest.fn(), equals: jest.fn() })),
         validationErrors: jest.fn(() => true)
       })
       const res = httpMocks.createResponse()
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.postReset(req, res)
 
       expect(res._getRedirectUrl()).toBe('back')
     })
 
-    it('should redirect to back when password reset token is invalid', async () => {
-      // const user = { username: 'test' }
-      const mockAuthService = {
-        resetPassword: jest.fn(() => false)
-      }
+    it('should redirect to back if password reset token is invalid', async () => {
+      const { authService } = setup()
+      authService.resetPassword = jest.fn(() => Promise.resolve(false))
 
       const req = httpMocks.createRequest({
         method: 'POST',
         url: '/password/reset',
         body: { password: 'test@example.com', confirmPassword: 'test@example.com' },
-        login: jest.fn(),
+        login: jest.fn((user, callback) => callback()),
         flash: jest.fn(),
         assert: jest.fn(() => ({ len: jest.fn(), equals: jest.fn() })),
-        validationErrors: jest.fn(() => true)
+        validationErrors: jest.fn(() => false)
       })
       const res = httpMocks.createResponse()
 
-      const passwordController = PasswordController({ authService: mockAuthService })
+      const passwordController = PasswordController({ authService })
       await passwordController.postReset(req, res)
 
-      expect(res._getRedirectUrl()).toBe('back')
+      expect(res._getRedirectUrl()).toBe('/password/forgot')
     })
   })
 })
