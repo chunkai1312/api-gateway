9e58397723caec5cbb64d6f15f0686636b9e931c
Chun-Kai Wang <chunkai1312@gmail.com>
2017-06-30 15:51:02 +0800

    test(controllers): home

--- test/controllers/home.spec.js
+++ test/controllers/home.spec.js
@@ -2,6 +2,13 @@ import httpMocks from 'node-mocks-http'
 import HomeController from '../../src/controllers/home'
 
 describe('HomeController', () => {
+  describe('#HomeController()', () => {
+    it('should create a HomeController', () => {
+      const homeController = HomeController()
+      expect(homeController).toBeInstanceOf(Object)
+    })
+  })
+
   describe('#index()', () => {
     it('should respond 200 with user data', async () => {
       const req = httpMocks.createRequest({ method: 'GET', url: '/' })
