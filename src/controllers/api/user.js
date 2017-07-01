92e5cb5294db195a921318e97c39dc2ae50a04af
Chun-Kai Wang <chunkai1312@gmail.com>
2017-07-01 20:56:29 +0800

    refactor(controllers): user

--- src/controllers/api/user.js
+++ src/controllers/api/user.js
@@ -1,8 +1,9 @@
 import error from 'http-errors'
-import { Types } from 'mongoose'
-import OAuthUser from '../../models/oauth_user'
+import UserService from '../../services/oauth/user'
 
-function UserController (dependencies = {}) {
+function UserController (dependencies = { userService: UserService() }) {
+  const { userService } = dependencies
+
   const userController = {}
 
   /**
@@ -10,7 +11,7 @@ function UserController (dependencies = {}) {
    * Find all users.
    */
   userController.index = async (req, res) => {
-    const users = await OAuthUser.find()
+    const users = await userService.getClients()
     res.status(200).json(users)
   }
 
@@ -19,13 +20,7 @@ function UserController (dependencies = {}) {
    * Create a new user.
    */
   userController.create = async (req, res) => {
-    const userByUsername = await OAuthUser.findByUsername(req.body.username)
-    if (userByUsername) return res.status(400).json({ message: 'Username already exists' })
-
-    const userByEmail = await OAuthUser.findByEmail(req.body.email)
-    if (userByEmail) return res.status(400).json({ message: 'Email already in use' })
-
-    const user = await OAuthUser.create(req.body)
+    const user = await userService.createClient(req.body)
     res.status(201).json(user)
   }
 
@@ -34,8 +29,7 @@ function UserController (dependencies = {}) {
    * Find one user by ID.
    */
   userController.show = async (req, res) => {
-    if (!Types.ObjectId.isValid(req.params.id)) throw error(404)
-    const user = await OAuthUser.findById(req.params.id)
+    const user = await userService.getClientById(req.params.id)
     if (!user) throw error(404)
     res.status(200).json(user)
   }
@@ -45,8 +39,7 @@ function UserController (dependencies = {}) {
    * Update an existing user by ID.
    */
   userController.update = async (req, res) => {
-    if (!Types.ObjectId.isValid(req.params.id)) throw error(404)
-    const user = await OAuthUser.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    const user = await userService.updateClient(req.params.id, req.body)
     if (!user) throw error(404)
     res.status(200).json(user)
   }
@@ -56,25 +49,9 @@ function UserController (dependencies = {}) {
    * Destroy an existing user by ID.
    */
   userController.destroy = async (req, res) => {
-    if (!Types.ObjectId.isValid(req.params.id)) throw error(404)
-    const user = await OAuthUser.findByIdAndRemove(req.params.id)
+    const user = await userService.deleteClient(req.params.id)
     if (!user) throw error(404)
     res.status(200).json({ id: user.id, deleted: true })
-  }
-
-  /**
-   * PUT /oauth2/users/:id/password
-   * Change password of the user.
-   */
-  userController.changePassword = async (req, res) => {
-    if (!Types.ObjectId.isValid(req.params.id)) throw error(404)
-    const user = await OAuthUser.findById(req.params.id)
-    if (!user) throw error(404)
-    const isAuthenticated = await user.authenticate(req.body.oldPassword)
-    if (!isAuthenticated) throw error(400)
-    user.password = req.body.newPassword
-    await user.save()
-    res.status(204).end()
   }
 
   return userController
